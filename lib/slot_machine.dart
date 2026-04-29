35f55a804fd0d8ac5e85a2d462eb12c322d67f8d
whypikmi <ttelatnikova1@yandex.ru>
2026-04-29 15:22:49 +0300

    Update SoundService to use audioplayers for Web and Android

--- lib/slot_machine.dart
+++ lib/slot_machine.dart
@@ -38,7 +38,7 @@ class _SlotMachineState
 
   Future<void> _spin() async {
     if (_coins <= 0 || _isSpinning) return;
-    SoundService.playClick();
+    await SoundService.playClick();
     setState(() {
       _isSpinning = true;
       _message = '';
@@ -65,26 +65,29 @@ class _SlotMachineState
     await Future.delayed(
       Duration(milliseconds: 300),
     );
+    String newMessage;
+    int coinsChange;
+    if (result1 == result2 &&
+        result2 == result3) {
+      if (result1 == 'assets/images/seven.png') {
+        coinsChange = 10;
+        newMessage = 'ДЖЕКПОТ! 🎰🎰🎰 +10 монет';
+        await SoundService.playJackpot();
+      } else {
+        coinsChange = 3;
+        newMessage = 'Победа! 🎆 +3 монеты';
+        await SoundService.playWin();
+      }
+    } else {
+      coinsChange = -1;
+      newMessage =
+          'Попробуй ещё раз 😔 -1 монета';
+      await SoundService.playLose();
+    }
     setState(() {
       _isSpinning = false;
-      if (result1 == result2 &&
-          result2 == result3) {
-        if (result1 ==
-            'assets/images/seven.png') {
-          _coins += 10;
-          _message = 'ДЖЕКПОТ! 🎰🎰🎰 +10 монет';
-          SoundService.playJackpot();
-        } else {
-          _coins += 3;
-          _message = 'Победа! 🎆 +3 монеты';
-          SoundService.playWin();
-        }
-      } else {
-        _coins -= 1;
-        _message =
-            'Попробуй ещё раз 😔 -1 монета';
-        SoundService.playLose();
-      }
+      _coins += coinsChange;
+      _message = newMessage;
     });
   }
 
