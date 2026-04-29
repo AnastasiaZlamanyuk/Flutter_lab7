ffa743c5f7fd0370ca13807bd4b190a9d925638b
whypikmi <ttelatnikova1@yandex.ru>
2026-04-29 15:00:27 +0300

    Bonus: add sound effects via Web Audio API

--- lib/slot_machine.dart
+++ lib/slot_machine.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'package:slot_machine/slot_row.dart';
+import 'sound_service.dart';
 
 class SlotMachine extends StatefulWidget {
   const SlotMachine({super.key});
@@ -37,10 +38,15 @@ class _SlotMachineState
 
   Future<void> _spin() async {
     if (_coins <= 0 || _isSpinning) return;
+    SoundService.playClick();
     setState(() {
       _isSpinning = true;
       _message = '';
     });
+    if (!_backgroundStarted) {
+      SoundService.playBackground();
+      _backgroundStarted = true;
+    }
     final result1 = await _spinReel(
       totalTicks: 10,
       onTick: (val) =>
@@ -67,14 +73,17 @@ class _SlotMachineState
             'assets/images/seven.png') {
           _coins += 10;
           _message = 'ДЖЕКПОТ! 🎰🎰🎰 +10 монет';
+          SoundService.playJackpot();
         } else {
           _coins += 3;
           _message = 'Победа! 🎆 +3 монеты';
+          SoundService.playWin();
         }
       } else {
         _coins -= 1;
         _message =
             'Попробуй ещё раз 😔 -1 монета';
+        SoundService.playLose();
       }
     });
   }
@@ -90,11 +99,37 @@ class _SlotMachineState
     });
   }
 
+  void _toggleMute() {
+    SoundService.toggleMute();
+    setState(() {
+      _isMuted = SoundService.isMuted;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
+        Align(
+          alignment: Alignment.topRight,
+          child: Padding(
+            padding: EdgeInsets.only(
+              right: 16,
+              top: 8,
+            ),
+            child: IconButton(
+              onPressed: _toggleMute,
+              icon: Icon(
+                _isMuted
+                    ? Icons.volume_off
+                    : Icons.volume_up,
+                color: Colors.white,
+                size: 28,
+              ),
+            ),
+          ),
+        ),
         Text(
           '💰 Монеты: $_coins',
           style: TextStyle(
@@ -175,6 +210,12 @@ class _SlotMachineState
     );
   }
 
+  @override
+  void initState() {
+    super.initState();
+    SoundService.playBackground();
+  }
+
   final _random = Random();
   final _symbols = [
     'assets/images/cherry.png',
@@ -187,4 +228,6 @@ class _SlotMachineState
   var _slot3 = 'assets/images/seven.png';
   var _message = '';
   var _isSpinning = false;
+  var _isMuted = false;
+  var _backgroundStarted = false;
 }
